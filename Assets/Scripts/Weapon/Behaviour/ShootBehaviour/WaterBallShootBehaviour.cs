ddb6a1a927d2c4b844ed9e4531be6c633a55f658
lxderunner <daniil.k@22bet.com>
2022-08-01 17:59:24 +0300

    Water bullet.

---
+++ Assets/Scripts/Weapon/Behaviour/ShootBehaviour/WaterBallShootBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Weapon.Component;
+
+namespace Weapon.Behaviour
+{
+    public class WaterBallShootBehaviour : PistolShootBehaviour
+    {
+        
+    }
+}
